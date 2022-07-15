5e4dc77808d038399c1fa0b0d8a40350300a488c
alexiva10 <109339928+alexiva10@users.noreply.github.com>
2022-07-15 11:01:24 +0300

    Add files via upload

---
+++ SwiftUI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SlotsApp.swift
+//  SwiftUI Slots
+//
+//  Created by Alex Ivanescu on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
